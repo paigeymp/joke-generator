aa715f66296df3f53d00a907b3e7b35a562184db
Paige Powell <117302610+paigeymp@users.noreply.github.com>
2024-11-28 11:57:23 -0700

    scaffolding generator

---
+++ src/components/Joke.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Button from "./Button";
+
+const Joke = () => {
+  return (
+    <div classNae="Joke">
+      <Button />
+    </div>
+  );
+};
+
+export default Joke;
